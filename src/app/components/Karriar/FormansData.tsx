f7cff5f2c3e3fd6511c91ed8fca932e3df3df55d
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2023-03-22 20:30:03 +0100

    Add data

---
+++ src/app/components/Karriar/FormansData.tsx
@@ -0,0 +1,52 @@
+const formansData = [
+    {
+        title: "Friskvård",
+        image: "https://img.icons8.com/ios-filled/50/000000/stretching.png",
+        description:
+            "Motsvarande ett årskort på Sats"
+    },
+    {
+        title: "Rikskortet",
+        image: "https://img.icons8.com/external-vitaliy-gorbachev-fill-vitaly-gorbachev/60/000000/external-credit-card-ecommerce-vitaliy-gorbachev-fill-vitaly-gorbachev-1.png",
+        description:
+            "30% av 2200kr"
+    },
+    {
+        title: "Semester",
+        image: "https://img.icons8.com/ios-filled/50/000000/sunbathe.png",
+        description:
+            "Flexibelt"
+    },
+    {
+        title: "Utrustning",
+        image: "https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-code-coding-kiranshastry-solid-kiranshastry-2.png",
+        description:
+            "Ny dator och mobil var tredje år"
+    },
+    {
+        title: "Tjänstepension",
+        image: "https://img.icons8.com/glyph-neue/64/000000/money-box.png",
+        description: "Enligt ITP 1"
+    },
+    {
+        title: "Löneväxlning Pension",
+        image: "https://img.icons8.com/ios-filled/50/000000/cash-in-hand.png",
+        description: "Möjlighet att löneväxla mot pension"
+    },
+    {
+        title: "Bruttolöneavdrag",
+        image: "https://img.icons8.com/external-vitaliy-gorbachev-fill-vitaly-gorbachev/60/000000/external-desk-support-vitaliy-gorbachev-fill-vitaly-gorbachev.png",
+        description: "Utrusta ditt hemkontor med bruttolöneavdrag"
+    },
+    {
+        title: "Konferenser",
+        image: "https://img.icons8.com/ios-filled/50/000000/conference.png",
+        description: "Budget för konferenser"
+    }, {
+        title: "Utbildning",
+        image: "https://img.icons8.com/ios-filled/50/000000/laboratory.png",
+        description: "Utbildning och certifierings",
+    }
+];
+
+export default formansData;
