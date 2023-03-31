df31e4da71f13382d1036d471a165f73974d0d6c
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2023-03-31 18:12:07 +0200

    Remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Hero from "@/app/components/Home/Hero";
 import Intro from "@/app/components/Intro/Intro";
-import Erbjudande from "@/app/components/Home/Erbjudande";
 import Kunder from "@/app/components/Home/Kunder";
 import { BlogInlagg } from "@/app/components/Home/SenasteInlagg";
-import Team from "@/app/components/Home/Team";
 import Script from "next/script";
 import VideoWithOverlay from "@/app/components/Home/VideoOverlay";
 import WhatWeDo from "@/app/components/Team/WhatWeDo";
