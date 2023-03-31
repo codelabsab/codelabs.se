130fd03c830c3e97a37c3facf7b2bafc7d478a84
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2023-03-31 19:58:27 +0200

    Image is better than img

--- src/app/components/Karriar/Tjanster.tsx
+++ src/app/components/Karriar/Tjanster.tsx
@@ -287,8 +287,11 @@ export default function Tjanster() {
 
                 {/* Image section */}
                 <div className="mt-32 sm:mt-40 xl:mx-auto xl:max-w-7xl xl:px-8">
-                    <img
+                    <Image
                         src="https://images.unsplash.com/photo-1529156069898-49953e39b3ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
+                        width={2832}
+                        height={1880}
+                        priority={true}
                         alt=""
                         className="aspect-[5/2] w-full object-cover xl:rounded-3xl"
                     />
--- src/app/components/Team/WhatWeDo.tsx
+++ src/app/components/Team/WhatWeDo.tsx
@@ -34,13 +34,6 @@ export default function WhatWeDo() {
                         </div>
                         <div
                             className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
-                            {/*<div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">*/}
-                            {/*    <img*/}
-                            {/*        src="/assets/entourage.jpeg"*/}
-                            {/*        alt=""*/}
-                            {/*        className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"*/}
-                            {/*    />*/}
-                            {/*</div>*/}
                             <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
                                 <Image
                                     src="/assets/aveqia.jpeg"
