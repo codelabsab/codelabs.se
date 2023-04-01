d3b5230644ef05856f40409e1ed9d5e02ff34ac5
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2023-04-01 12:02:46 +0200

    Update team data

--- src/app/components/Data/TeamData.tsx
+++ src/app/components/Data/TeamData.tsx
@@ -1,9 +1,9 @@
 export const people = [
   {
     name: 'Honey Helensdotter',
-    role: 'Business Developer',
-    imageUrl: "assets/team/honey.jpeg",
-    bio: 'Honey är den drivna affärsutvecklaren som ser till att saker blir gjorda. En stjärna på branding, rekrytering och att se till alla konsulter mår bäst.',
+    role: 'Organization Developer',
+    imageUrl: "/assets/team/honey.jpeg",
+    bio: 'Honey tar oss till nya höjder. En stjärna på branding, rekrytering och att se till alla vi rör oss åt rätt håll.',
     skills: 'business development strategy sales recruitment',
     github_url: `#`
   },
@@ -12,7 +12,7 @@ export const people = [
     role: `DevOps Engineer`,
     skills: `python terraform argo k8s cloud`,
     bio: `Mr One shot, one kill. En legendarisk Devops-ingenjör som levererar resultat. Pär är en av våra mest erfarna konsulter och har en stor passion för att utveckla och automatisera. Han gillar även att åka mountain-bike nedför väldigt branta backar.`,
-    imageUrl: "assets/team/paer.jpeg",
+    imageUrl: "/assets/team/paer.jpeg",
     github_url: `https://github.com/parberge`,
   },
   {
@@ -20,7 +20,7 @@ export const people = [
     role: `Software Engineer`,
     bio: `El Rancho, drogbaronen från Mexiko som för närvarande gömmer sig i Sverige och lever under identiteten Kami, en av grundarna till detta fantastiska bolag.`,
     skills: `python node js terraform k8s argocd cloud`,
-    imageUrl: "assets/team/kami.jpeg",
+    imageUrl: "/assets/team/kami.jpeg",
     github_url: `https://github.com/kamigerami`,
   },
   {
@@ -28,7 +28,7 @@ export const people = [
     role: `Software Engineer`,
     bio: `Förvirrad pappa på vift som gillar tech, skapa saker och att hänga med teamet. Chief nerd på bolaget och en av grundarna.`,
     skills: `python terraform kubernetes ci/cd argocd aws gcp azure`,
-    imageUrl: "assets/team/tommy.jpeg",
+    imageUrl: "/assets/team/tommy.jpeg",
     github_url: `https://github.com/tommymalmqvist`,
   },
   {
@@ -36,7 +36,7 @@ export const people = [
     role: `Software Developer`,
     bio: `Utvecklaren som alltid levererar. Smart, analytisk och en riktig team player. Man kan alltid lita på att Carl-Fredrik levererar.`,
     skills: `js/ts react node python ci/cd`,
-    imageUrl: "assets/team/carl-fredrik.jpeg",
+    imageUrl: "/assets/team/carl-fredrik.jpeg",
     github_url: `https://github.com/cfarvidson`,
   },
   {
@@ -44,7 +44,7 @@ export const people = [
     role: `DevOps Engineer`,
     bio: `Mannen som har fler timmar på youtube än vad han har sovit. En av våra mest erfarna youtubers som levererar memes till resten av bolaget.`,
     skills: ` python ansible puppet docker openshift ci/cd`,
-    imageUrl: "assets/team/mauricio.jpeg",
+    imageUrl: "/assets/team/mauricio.jpeg",
     github_url: `https://github.com/marresgit`,
   }
 ]
