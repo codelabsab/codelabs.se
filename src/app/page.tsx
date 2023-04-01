b5bb5f812b013317b193ebfff84a1b2cf14a0d39
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2023-04-01 12:00:11 +0200

    Indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,31 @@
 import Intro from "@/app/components/Intro/Intro";
 import Kunder from "@/app/components/Home/Kunder";
-import { BlogInlagg } from "@/app/components/Home/SenasteInlagg";
+import {BlogInlagg} from "@/app/components/Home/SenasteInlagg";
 import Script from "next/script";
 import VideoWithOverlay from "@/app/components/Home/VideoOverlay";
 import WhatWeDo from "@/app/components/Team/WhatWeDo";
 
 export default function Home() {
-  return (
-    <main>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-HZZ215K6CG"></Script>
-      <Script
-        id="ganalytics"
-        dangerouslySetInnerHTML={{
-          __html: `
+    return (
+        <main>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-HZZ215K6CG"></Script>
+            <Script
+                id="ganalytics"
+                dangerouslySetInnerHTML={{
+                    __html: `
     window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-HZZ215K6CG');
     `,
-        }}
-      />
-
-      <VideoWithOverlay />
-      <Intro />
-      <WhatWeDo />
-      <Kunder />
-      <BlogInlagg />
-    </main>
-  );
+                }}
+            />
+            <VideoWithOverlay/>
+            <Intro/>
+            <WhatWeDo/>
+            <Kunder/>
+            <BlogInlagg/>
+        </main>
+    );
 }
